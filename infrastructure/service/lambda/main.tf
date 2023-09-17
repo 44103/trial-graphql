40db2fd93a7dcec96cdf74eff6f3b4e8e2fab376
Kenko Yoshitomi <kyoshitomi.cursor@gmail.com>
2023-09-17 09:03:39 +0900

    Configure resolvers

--- infrastructure/service/lambda/main.tf
+++ infrastructure/service/lambda/main.tf
@@ -1,7 +1,46 @@
 data "aws_caller_identity" "_" {}
 
-module "lambda_blog" {
+module "lambda" {
   source  = "../../modules/lambda"
   commons = local.commons
   name    = "blog"
+}
+
+module "appsync" {
+  source      = "../../modules/appsync"
+  commons     = local.commons
+  name        = "blog"
+  schema_file = "./schema.graphql"
+  lambda      = module.lambda
+  data_source = {
+    lambda = {
+      blog = module.lambda
+    }
+  }
+  resolvers = {
+    getPost = {
+      type        = "Query"
+      data_source = "blog"
+      kind        = "UNIT"
+      code        = "../../functions/appsync/resolver/lambda.js"
+    }
+    allPosts = {
+      type        = "Query"
+      data_source = "blog"
+      kind        = "UNIT"
+      code        = "../../functions/appsync/resolver/lambda.js"
+    }
+    addPost = {
+      type        = "Mutation"
+      data_source = "blog"
+      kind        = "UNIT"
+      code        = "../../functions/appsync/resolver/lambda.js"
+    }
+    relatedPosts = {
+      type        = "Post"
+      data_source = "blog"
+      kind        = "UNIT"
+      code        = "../../functions/appsync/resolver/lambda.js"
+    }
+  }
 }
